bb21302996599c140ab64d215d6a9f637f62acb7
AdkCoderz <akshaykurveak@gmail.com>
2025-07-16 18:47:11 +0530

    creating first monorepo

---
+++ my-next-app/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@mon-practice/my-next-app',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/my-next-app',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
